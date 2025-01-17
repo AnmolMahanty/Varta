a64ac5a0177ed30bfc88db7403bd20de2cd68a8e
Anmol Mahanty <mahantyanmol14@gmail.com>
2025-01-17 13:07:41 +0530

    Profile ,setting pages and authentication done
    Update profile feature , change theme on application feature , complete authentication feature done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -93,25 +93,29 @@ export const logout = (req, res) => {
 export const updateProfile = async (req, res) => {
   try {
     const {profilePic} = req.body;
-    const user=req.user._id;
+    const userId=req.user._id;
 
     if(!profilePic){
       return res.status(400).json({message:"Profile pic is required"})
     }
 
-    await cloudinary.upload(profilePic)
-    const updateuser = await User.findByIdAndUpdate(userId, {profilePic:uploadedResponse.secure_url},{new:true})
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
 
-    res.status(200).json(updateuser)
+    res.status(200).json(updatedUser);
   } catch (error) {
-    console.log("Error in updateProfile controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
 export const checkAuth = (req, res) => {
   try {
-    res.status(200).json(req.user)
+    res.status(200).json(req.user);
   } catch (error) {
     console.log("Error in checkAuth controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
